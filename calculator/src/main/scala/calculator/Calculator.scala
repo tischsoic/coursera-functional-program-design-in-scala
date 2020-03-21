809b3b1116acf5eac0569662923e6da5772f515a
Jakub Brzegowski <symfiz@gmail.com>
2020-03-21 19:41:29 +0100

    Add solution to calculator

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -11,12 +11,18 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator extends CalculatorInterface {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+    namedExpressions.transform((name, s) => Var(eval(s(), namedExpressions.removed(name))))
   }
 
-  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
-  }
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double =
+    expr match {
+      case Literal(v) => v
+      case Ref(name) => if (references.contains(name)) eval(references(name)(), references) else Double.NaN
+      case Plus(a, b) => eval(a, references) + eval(b, references)
+      case Minus(a, b) => eval(a, references) - eval(b, references)
+      case Times(a, b) => eval(a, references) * eval(b, references)
+      case Divide(a, b) => eval(a, references) / eval(b, references)
+    }
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,19 @@ package calculator
 object Polynomial extends PolynomialInterface {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Var(b() * b() - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    val delta = computeDelta(a, b, c)
+    Var(
+      if (delta() < 0) Set()
+      else if (delta() == 0) Set(-b() / (2 * a()))
+      else Set(
+        (-b() - Math.sqrt(delta()))/ (2 * a()),
+        (-b() + Math.sqrt(delta()))/ (2 * a())
+      )
+    )
   }
 }
